44c25b35128a3981acc35c521c0859b7dbdbe14d
Naresh goud <ediganareshgoud406@gmail.com>
2025-08-22 19:59:32 +0530

    map

--- app.js
+++ app.js
@@ -13,13 +13,18 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function (socket) {
-  console.log("connected");
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
 });
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("server running successfully");
 });
